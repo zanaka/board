b5f988a0e675f0bb18642dee890f06ffac09dc9c
zanaka <e155752@ie.u-ryukyu.ac.jp>
2018-02-28 13:36:24 +0900

    掲示板画面フォームと入力フォームのルーティングの追加

--- src/routes.php
+++ src/routes.php
@@ -5,6 +5,32 @@ use Slim\Http\Response;
 
 // Routes
 
+// 掲示板全体画面
+$app->get('/board', function (Request $request, Response $response) {
+    return $this->renderer->render($response, 'board.phtml');
+});
+
+//掲示板(上)　内容入力フォーム
+$app->post('/board', function (Request $request, Response $response) {
+    $subject = $request->getParsedBodyParam('subject');
+    $subject = $request->getParsedBodyParam('subject');
+    // ここに保存の処理を書く
+    $sql1 = 'INSERT INTO message (body) values (:subject)';
+    $sql2 = 'INSERT INTO message (user_name) values (:subject)';
+    $stmt = $this->db->prepare($sql);
+    $result = $stmt->execute(['subject' => $subject]);
+    if (!$result) {
+        throw new \Exception('could not save the ticket');
+    }
+
+    // 保存が正常にできたら一覧ページへリダイレクトする
+    return $response->withRedirect("/board");
+});
+
+
+
+
+
 $app->get('/[{name}]', function (Request $request, Response $response, array $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
