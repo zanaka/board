7541f0bdd4eb1153957254b4ed1726bae76d8fec
zanaka <e155752@ie.u-ryukyu.ac.jp>
2018-03-03 15:51:50 +0900

    データベース接続情報の追加

---
+++ templates/connect/PDO_testdb.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>PDOでデータベースに接続する</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+
+<body>
+<div>
+    <!-- DB情報 -->
+    <?php
+    $user = 'testuser';
+    $password = 'pw4testuser';
+    $dbName = 'testdb';
+    $host = 'localhost:8889';
+    $dsn = "mysql:host={$host};dbname={$dbName};charset=utf8";
+
+    å
+
+
+
+    ?>
+
+
+</body>
+</html>
